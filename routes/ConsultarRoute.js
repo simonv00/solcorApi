244dcaf07978c01ed232675450420e2754600d1e
Simon Valencia Ochoa <simonvalencia305@gmail.com>
2022-10-18 11:09:41 -0500

    Update ConsultarRoute.js

--- routes/ConsultarRoute.js
+++ routes/ConsultarRoute.js
@@ -7,7 +7,7 @@ route.all("/", function (req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
-//holi
+
 route.post("/", Controller.calculate);
 route.get("/", Controller.getAll)
 
